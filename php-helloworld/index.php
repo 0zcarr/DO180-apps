ecb126f0692b142546a18300158fe5efb3500f89
Student User <student@workstation.lab.example.com>
2021-06-18 16:00:19 -0400

    This will change all the other guys

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming This is a q test\n";
-print "Another change will appear after the update \n";
+print "Pushing something totally different \n "; 
 ?>
